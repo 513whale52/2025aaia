47d77d708c31b603467e5b37ebf059b25040db5d
513whale52 <13750545@me.mcu.edu.tw>
2025-03-14 12:10:33 +0800

    Add files via upload

---
+++ wweek04/week-4.cpp
@@ -0,0 +1,17 @@
+///week04-3.cpp
+//for°j°é
+#include <stdio.h>
+
+int main()
+{
+    int N=5;
+    for(int i=0;i<N,i++){
+       printf("%d",i);
+    }
+    printf("\n");
+
+    for(int i=N-1; i>=0; i--){
+        printf("%d",i);
+
+    }
+}
