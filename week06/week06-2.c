6027f29ebab9d6ab7af16e4d35c75d06869cfcdc
513whale52 <13750545@me.mcu.edu.tw>
2025-05-23 01:50:27 +0800

    Add files via upload

---
+++ week06/week06-2.c
@@ -0,0 +1,16 @@
+///week06-2.cpp
+#include <stdio.h>
+
+int main()
+(
+
+    if(999)printf("999 good 成立\n");
+    if(3)printf("3 good 成立\n");
+    if(2)printf("2 good 成立\n");
+    if(1)printf("1 good 成立\n");
+    if(0)printf("0 good 成立\n");
+    if(-1)printf("-1 good 成立\n");
+    if(-2)printf("-2 good 成立\n");
+    while(1) printf("1good成立\n");
+
+)
